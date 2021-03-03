66735905d4d09ac48c135a8380aca656d138546e
William Bugenis <wbugenis@gmail.com>
2021-03-03 11:16:35 -0500

    adv deliverables done, code refactored

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-// write your code here
+//DB URLS
 const spiceUrl = 'http://localhost:3000/spiceblends'
 const ingredientUrl = 'http://localhost:3000/ingredients'
+
+//Elements to Update
+const spiceImgs = document.querySelector("div#spice-images")
 const spiceDetail = document.querySelector("div#spice-blend-detail")
 const spiceTitle = spiceDetail.querySelector("h2.title")
 const list = spiceDetail.querySelector("ul.ingredients-list")
 
+//Forms to listen on
 const updateForm = document.querySelector("form#update-form")
 const ingredientForm = document.querySelector("form#ingredient-form")
 
 //Show details of spice object on webpage
 function showSingleSpice(spice){
-
+    
     const img = spiceDetail.querySelector("img.detail-image")
     img.src = spice.image
     img.alt = spice.title
-
-    spiceTitle.textContent = spice.title
-
+    spiceTitle.innerText = ''
+    spiceTitle.innerText = spice.title
+    list.innerHTML = ''
     
     spice.ingredients.forEach(ingredient => {
         const li = document.createElement('li')
@@ -29,19 +33,38 @@ function showSingleSpice(spice){
     ingredientForm.dataset.id = spice.id
 }
 
-//Fetch details for first spice
-fetch(`${spiceUrl}/1`)
-    .then(response => response.json())
-    .then(spice => showSingleSpice(spice))
-    .catch(error => console.log(error))
+//Fetch details for spice based on id argument and show in detail view
+function fetchSpice(id){
+    fetch(`${spiceUrl}/${id}`)
+        .then(response => response.json())
+        .then(spice => showSingleSpice(spice))
+        .catch(error => console.log(error))
+}
 
-//Listen for submission on update form, update spice detail after response
-updateForm.addEventListener('submit', event => { 
+//Make images from spice object and append to spice-imgs div
+function makeSpiceImg(spice) {
+    const img = document.createElement('img')
+    img.dataset.id = spice.id
+    img.src = spice.image
+    img.alt = spice.title
+    spiceImgs.append(img)
+}
+
+//Get all spices and send to img creation helper function
+function fetchAllSpices(){
+    fetch(spiceUrl)
+        .then(response => response.json())
+        .then(spices => spices.forEach(makeSpiceImg))
+        .catch(error => console.log(error))
+}
+
+//Handle spice title update
+function updateSpiceTitle(event){
     event.preventDefault()
 
     const title = event.target.title.value
-
-    fetch(`${spiceUrl}/${updateForm.dataset.id}`, {
+    const id = updateForm.dataset.id
+    fetch(`${spiceUrl}/${id}`, {
         method: 'PATCH',
         headers: {
             'Content-Type':'application/json',
@@ -49,31 +72,47 @@ updateForm.addEventListener('submit', event => {
         },
         body: JSON.stringify({title})
     })
-    .then(response => response.json())
-    .then(updatedSpice => spiceTitle.textContent = updatedSpice.title)
-    .catch(error => console.log(error))
+        .then(response => response.json())
+        .then(spice => fetchSpice(id))
+        .catch(error => console.log(error))
     updateForm.reset()
-})
+}
 
-//Listen for submission on new ingredients form, display on page
-ingredientForm.addEventListener('submit', event => {
+//Handle ingredient form submission
+function addIngredient(event){ 
     event.preventDefault()
     const name = event.target.name.value
-    const li = document.createElement('li')
-    li.textContent = name
-    list.append(li)
 
-    // const spiceBlendId = ingredientForm.dataset.id
-    // fetch(`${ingredientUrl}/${spiceBlendId}`, {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type':'application/json',
-    //         'Accept':'application/json'
-    //     },
-    //     body: JSON.stringify({ingredients:[name, spiceBlendId]})
-    // })
-    // .then(response => response.json())
-    // .then(spice => console.log(spice))
-    // .catch(error => console.log(error))
+    const spiceblendId = parseInt(ingredientForm.dataset.id)
+    fetch(ingredientUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({name, spiceblendId})
+    })
+        .then(response => response.json())
+        .then(fetchSpice(spiceblendId))
+        .catch(error => console.log(error))
     ingredientForm.reset()
+}
+
+//Listen for submission on update form, send to helper method to update
+updateForm.addEventListener('submit', updateSpiceTitle)
+
+//Listen for submission on new ingredients form, display on page
+ingredientForm.addEventListener('submit', addIngredient)
+
+//Listen for clicks on spiceImgs div, if image clicked, send img's spice ID to helper method to fetch and show
+spiceImgs.addEventListener('click', event => {
+    if(event.target.matches('img')){
+        fetchSpice(event.target.dataset.id)
+    }
 })
+
+// populate spiceImgs
+fetchAllSpices()
+
+//Show first spice in detail view
+fetchSpice(1)
