2e5aa7441a875f997826c011ddf70bbeb086706b
William Bugenis <wbugenis@gmail.com>
2021-03-03 09:36:43 -0500

    1st deliv - display spice done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // write your code here
+const url = 'http://localhost:3000/spiceblends/1'
+const spiceDetail = document.querySelector("div#spice-blend-detail")
+
+function showSingleSpice(spice){
+    const img = spiceDetail.querySelector("img.detail-image")
+    img.src = spice.image
+    img.alt = spice.title
+    spiceDetail.querySelector("h2.title").textContent = spice.title
+    const list = spiceDetail.querySelector("ul.ingredients-list")
+    spice.ingredients.forEach(ingredient => {
+        console.log(ingredient)
+        const li = document.createElement('li')
+        li.textContent = ingredient.name
+        list.append(li)
+    })
+}
+
+fetch(url)
+    .then(response => response.json())
+    .then(spice => showSingleSpice(spice))
